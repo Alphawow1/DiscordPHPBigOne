d37d700e80e1a0500ab8d46177ac783cebd45f8e
Alphawow1 <59313183+Alphawow1@users.noreply.github.com>
2024-06-15 14:32:07 +0300

    Update simple_example.php

--- simple_example.php
+++ simple_example.php
@@ -4,7 +4,7 @@ require_once 'DiscordInteraction.php';
 use Discord\Interaction;
 use Discord\InteractionResponseType;
 
-$CLIENT_PUBLIC_KEY = getenv('CLIENT_PUBLIC_KEY');
+$CLIENT_PUBLIC_KEY = getenv('0f8ab6334fbbe0ec9ee562fd5a43ea1e8a80e8b52cc7734037897ea3b09e9d39');
 
 $signature = $_SERVER['HTTP_X_SIGNATURE_ED25519'];
 $timestamp = $_SERVER['HTTP_X_SIGNATURE_TIMESTAMP'];
